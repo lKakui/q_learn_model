6638c7b27326348eff5e451fa16b9da642f2e941
Luiz Pedro Kakuichi <lpkakuichi@ucs.local>
2025-06-09 21:07:48 -0300

    calculate_reward function

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import matriz as m
 
+inicial_position = {
+    (9, 4): {
+        "up": 0,
+        "down": 0,
+        "left": 0,
+        "right": 0,
+        "value": 0
+    }
+}
+final_postion = {
+    (5, 11): {
+        "up": 0,
+        "down": 0,
+        "left": 0,
+        "right": 0,
+        "value": 0
+    }
+}
+r = 0 # posição atual
+y = 0.5 # propagação
+
+
+#qtable = dict{key(tuple), value{dict{key(string), value(int)}}} 
+
+
 def main():
-    position = [9, 4]
     matrix = m.create_map()
-    m.run_model(source=matrix, probability start=position)
+
+    m.run_model(source=matrix, qtable=qtable, start=inicial_position, end=final_postion, propagation=y)
+
--- matriz.py
+++ matriz.py
@@ -10,10 +10,11 @@ positions = [
     [2, 9, -1],
     [3, 1, -1],
     [3, 8, -1],
-    [4, 11, 100],
     [5, 6, -1],
     [8, 6, -1],
 ]
+
+#TODO, tkinter
 
 null_zone = [
     [6, 0, 3],
@@ -28,15 +29,10 @@ def create_map():
     return matrix
 
 def fill_with_barriers(matrix):
-    for pos in positions:
-        row, col, val = pos
+    for row, col, val in positions:
         matrix[row][col] = val
 
-    for area in null_zone:
-        row = area[0]
-        start_col = area[1]
-        end_col = area[2]
-
+    for row, start_col, end_col in null_zone:
         for col in range(start_col, end_col + 1):
             matrix[row][col] = -1
 
@@ -49,4 +45,31 @@ def alloc_map():
         matrix.append(aux)
     return matrix
 
+def options(position, qtable):
+    return qtable.get(position, []) # return a list dict{key(tuple), value(key(string), value(int))}
+
+def calculate_reward(position, qtable, options_list, propagation):
+    value = 0
+    max_value = 0
+    for option in options_list:
+        if option in qtable:
+            option_value = qtable.get(option, 0)
+            if option_value > max_value:
+                max_value = option_value
+    value = propagation * position["value"] + max_value
+    return value
+
+def run_model(qtable, start, end, propagation):
+    position = start
+    while True: #TODO, revisar
+        options_list = options(position, qtable)
+        if not options_list:
+            print(f"No options available for position {position}.")
+            break
+        value = calculate_reward(position, qtable, options_list, propagation)
+        qtable.set(position, value)
+        
+        
+        position = options_list[0]
+        print(f"Moving to position {position}.")
 
